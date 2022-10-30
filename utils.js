2f49dab22384dc42dd1081e9a88e5c3be4752776
gurleenkr <gurleenkr123@gmail.com>
2022-10-31 02:31:05 +0530

    Minor Fix

--- utils.js
+++ utils.js
@@ -29,8 +29,10 @@ function updateUI(response) {
   var weatherInfo = getWeatherDetails();
   var weatherCode = hourly.weathercode[0];
   console.log(weatherCode);
-  var weatherDesc = weatherInfo.weatherCode.description;
-  var weatherIcon = weatherInfo.weatherCode.icon;
+  var weatherDesc = weatherInfo[weatherCode].description;
+  var weatherIcon = weatherInfo[weatherCode].icon;
+  console.log(weatherDesc);
+  console.log(weatherIcon);
 
   (document.querySelector('#lat').innerText = response.latitude);
   (document.querySelector('#long').innerText = response.longitude);
@@ -117,119 +119,117 @@ function getWeather(cloudHost, account, company, activity_id) {
 //
 function getWeatherDetails() {
   var json = {
-    "weather": {
-      "0": {
-        "description": "Clear Sky",
-        "icon": "./weather_code_icons/clear_sky.png"
-      },
-      "1": {
-        "description": "Mainly Clear",
-        "icon": "./weather_code_icons/cloudy.png"
-      },
-      "2": {
-        "description": "Partly Cloudy",
-        "icon": "./weather_code_icons/cloudy.png"
-      },
-      "3": {
-        "description": "Overcast",
-        "icon": "./weather_code_icons/cloudy.png"
-      },
-      "45": {
-        "description": "Fog",
-        "icon": "./weather_code_icons/foggy.png"
-      },
-      "48": {
-        "description": "Depositing Rime Fog",
-        "icon": "./weather_code_icons/foggy.png"
-      },
-      "51": {
-        "description": "Light Drizzle",
-        "icon": "./weather_code_icons/drizzle.png"
-      },
-      "53": {
-        "description": "Moderate Drizzle",
-        "icon": "./weather_code_icons/drizzle.png"
-      },
-      "55": {
-        "description": "Dense Drizzle",
-        "icon": "./weather_code_icons/drizzle.png"
-      },
-      "56": {
-        "description": "Light Freezing Drizzle",
-        "icon": "./weather_code_icons/freezing_drizzle_rain.png"
-      },
-      "57": {
-        "description": "Dense Freezing Drizzle",
-        "icon": "./weather_code_icons/freezing_drizzle_rain.png"
-      },
-      "61": {
-        "description": "Slight Rain",
-        "icon": "./weather_code_icons/rain.png"
-      },
-      "63": {
-        "description": "Moderate Rain",
-        "icon": "./weather_code_icons/rain.png"
-      },
-      "65": {
-        "description": "Heavy Rain",
-        "icon": "./weather_code_icons/rain.png"
-      },
-      "66": {
-        "description": "Light Freezing Rain",
-        "icon": "./weather_code_icons/freezing_drizzle_rain.png"
-      },
-      "67": {
-        "description": "Heavy Freezing Rain",
-        "icon": "./weather_code_icons/freezing_drizzle_rain.png"
-      },
-      "71": {
-        "description": "Slight Snowfall",
-        "icon": "./weather_code_icons/snow_fall.png"
-      },
-      "73": {
-        "description": "Moderate Snowfall",
-        "icon": "./weather_code_icons/snow_fall.png"
-      },
-      "73": {
-        "description": "Heavy Snowfall",
-        "icon": "./weather_code_icons/snow_fall.png"
-      },
-      "77": {
-        "description": "Snow Grains",
-        "icon": "./weather_code_icons/snow_grains.png"
-      },
-      "80": {
-        "description": "Slight Rain Showers",
-        "icon": "./weather_code_icons/rain_showers.png"
-      },
-      "81": {
-        "description": "Moderate Rain Showers",
-        "icon": "./weather_code_icons/rain_showers.png"
-      },
-      "82": {
-        "description": "Violent Rain Showers",
-        "icon": "./weather_code_icons/rain_showers.png"
-      },
-      "85": {
-        "description": "Slight Snow Showers",
-        "icon": "./weather_code_icons/snow_showers.png"
-      },
-      "86": {
-        "description": "Heavy Snow Showers",
-        "icon": "./weather_code_icons/snow_showers.png"
-      },
-      "95": {
-        "description": "Slight to Moderate Thunderstorm",
-        "icon": "./weather_code_icons/thunderstorm.png"
-      },
-      "96": {
-        "description": "Thunderstorm with Slight Hail",
-        "icon": "./weather_code_icons/thunderstorm_hail.png"
-      },
-      "99": {
-        "description": "Thunderstorm with Heavy Hail",
-        "icon": "./weather_code_icons/thunderstorm_hail.png"
-      }
+    "0": {
+      "description": "Clear Sky",
+      "icon": "./weather_code_icons/clear_sky.png"
+    },
+    "1": {
+      "description": "Mainly Clear",
+      "icon": "./weather_code_icons/cloudy.png"
+    },
+    "2": {
+      "description": "Partly Cloudy",
+      "icon": "./weather_code_icons/cloudy.png"
+    },
+    "3": {
+      "description": "Overcast",
+      "icon": "./weather_code_icons/cloudy.png"
+    },
+    "45": {
+      "description": "Fog",
+      "icon": "./weather_code_icons/foggy.png"
+    },
+    "48": {
+      "description": "Depositing Rime Fog",
+      "icon": "./weather_code_icons/foggy.png"
+    },
+    "51": {
+      "description": "Light Drizzle",
+      "icon": "./weather_code_icons/drizzle.png"
+    },
+    "53": {
+      "description": "Moderate Drizzle",
+      "icon": "./weather_code_icons/drizzle.png"
+    },
+    "55": {
+      "description": "Dense Drizzle",
+      "icon": "./weather_code_icons/drizzle.png"
+    },
+    "56": {
+      "description": "Light Freezing Drizzle",
+      "icon": "./weather_code_icons/freezing_drizzle_rain.png"
+    },
+    "57": {
+      "description": "Dense Freezing Drizzle",
+      "icon": "./weather_code_icons/freezing_drizzle_rain.png"
+    },
+    "61": {
+      "description": "Slight Rain",
+      "icon": "./weather_code_icons/rain.png"
+    },
+    "63": {
+      "description": "Moderate Rain",
+      "icon": "./weather_code_icons/rain.png"
+    },
+    "65": {
+      "description": "Heavy Rain",
+      "icon": "./weather_code_icons/rain.png"
+    },
+    "66": {
+      "description": "Light Freezing Rain",
+      "icon": "./weather_code_icons/freezing_drizzle_rain.png"
+    },
+    "67": {
+      "description": "Heavy Freezing Rain",
+      "icon": "./weather_code_icons/freezing_drizzle_rain.png"
+    },
+    "71": {
+      "description": "Slight Snowfall",
+      "icon": "./weather_code_icons/snow_fall.png"
+    },
+    "73": {
+      "description": "Moderate Snowfall",
+      "icon": "./weather_code_icons/snow_fall.png"
+    },
+    "73": {
+      "description": "Heavy Snowfall",
+      "icon": "./weather_code_icons/snow_fall.png"
+    },
+    "77": {
+      "description": "Snow Grains",
+      "icon": "./weather_code_icons/snow_grains.png"
+    },
+    "80": {
+      "description": "Slight Rain Showers",
+      "icon": "./weather_code_icons/rain_showers.png"
+    },
+    "81": {
+      "description": "Moderate Rain Showers",
+      "icon": "./weather_code_icons/rain_showers.png"
+    },
+    "82": {
+      "description": "Violent Rain Showers",
+      "icon": "./weather_code_icons/rain_showers.png"
+    },
+    "85": {
+      "description": "Slight Snow Showers",
+      "icon": "./weather_code_icons/snow_showers.png"
+    },
+    "86": {
+      "description": "Heavy Snow Showers",
+      "icon": "./weather_code_icons/snow_showers.png"
+    },
+    "95": {
+      "description": "Slight to Moderate Thunderstorm",
+      "icon": "./weather_code_icons/thunderstorm.png"
+    },
+    "96": {
+      "description": "Thunderstorm with Slight Hail",
+      "icon": "./weather_code_icons/thunderstorm_hail.png"
+    },
+    "99": {
+      "description": "Thunderstorm with Heavy Hail",
+      "icon": "./weather_code_icons/thunderstorm_hail.png"
     }
   }
 
